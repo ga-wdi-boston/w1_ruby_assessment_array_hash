e1519a8835a3da2a264a9a96cb05ecdd68dc4414
sarahchristine913 <s.mele13@gmail.com>
2014-06-11 10:20:55 -0400

    Mele_S Completed Assessment 2
    * Completed, with no testing or references

--- array_hash.rb
+++ array_hash.rb
@@ -1,4 +1,4 @@
-# Try to not use any reference for this one. If you need to use a reference, please note such in a comment below your answer. 
+# Try to not use any reference for this one. If you need to use a reference, please note such in a comment below your answer.
 # You'll still recieve credit, but its useful for me to know.
 
 # Question 1
@@ -6,15 +6,25 @@
 
 pets = ['cat', 'dog']
 
+pets.push('snake')
+
 # Question 2
 # Write code to return the value of the key :age in the friend hash
 friend = {name: "Chris", favorite_food: "Pizza", age: 27}
 
+friend[:age]
+
 # Question 3
 # Add a key of :hair_color with a value of "blue" to the friend array
+
+friend[hair_color: "blue"]
 
 # Question 4
 # Write code to retrieve the index of 'dog' in the pet array.
 
+pets.index('dog')
+
 # Question 5
 # Write code to change the value of :favorite_food in the friend array to 'sushi'
+
+friend[favorite_food: "sushi"]
