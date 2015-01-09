8ed741ad7ac3c33db8661d661c9e2d9d34d9dec0
fishermanswharff <fishermanswharff@github.com>
2015-01-09 16:16:15 -0500

    added rspec tests

--- lib/array_hash.rb
+++ lib/array_hash.rb
@@ -5,18 +5,31 @@
 # Question 1
 # On a new line, write code to insert the value `snake` at the end of the pets array.
 # Don't modify the original array creation line.
-
-pets = ['cat', 'dog']
+def append_to_array(value)
+  pets = ['cat', 'dog']
+end
 
 # Question 2
 # Write code to return the value of the key :age in the friend hash
-friend = { name: "Chris", favorite_food: "Pizza", age: 27 }
+def get_key_value(key)
+  friend = { name: "Chris", favorite_food: "Pizza", age: 27 }
+end
 
 # Question 3
 # Add a key of :hair_color with a value of "blue" to the friend hash
+def add_key_value_pair(key,value)
+  friend = { name: "Chris", favorite_food: "Pizza", age: 27 }
+end
 
 # Question 4
 # Write code to retrieve the index of 'dog' in the pet array
+def find_index_of_array(value)
+  pets = ['cat', 'dog']
+end
+
 
 # Question 5
 # Write code to change the value of :favorite_food in the friend hash to 'sushi'
+def change_value_of_key(key,value)
+  friend = { name: "Chris", favorite_food: "Pizza", age: 27 }
+end
