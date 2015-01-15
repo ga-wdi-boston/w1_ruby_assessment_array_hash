592d870551b6aa2c1dc52d60152b5739114fdad0
lady3bean <annehawleybrett@gmail.com>
2015-01-15 09:31:41 -0500

    assessment jan 15

--- lib/array_hash.rb
+++ lib/array_hash.rb
@@ -8,35 +8,37 @@
 # Don't modify the original array creation line.
 def append_to_array(value)
   pets = ['cat', 'dog']
-  # your answer here
+  pets.push 
 end
 
 # Question 2
 # Write code to return the value of the passed-in key from the friend hash.
 def get_key_value(key)
   friend = { name: "Chris", favorite_food: "Pizza", age: 27 }
-  # your answer here
+  return friend[:age]
 end
 
 # Question 3
 # Add a key and value to the friend hash.
 def add_key_value_pair(key,value)
   friend = { name: "Chris", favorite_food: "Pizza", age: 27 }
-  # your answer here
+  friend[:hair_color] = "blue"
+  return friend
 end
 
 # Question 4
 # Write code to retrieve the index of an element in the pet array.
 def find_index_of_array(value)
   pets = ['cat', 'dog']
-  # your answer here
+  pets.index(value) 
 end
 
 
-# Question 5
+# Question 5q
 # Write code to change the value of a key in the friend hash to a different
 # value.
 def change_value_of_key(key,value)
   friend = { name: "Chris", favorite_food: "Pizza", age: 27 }
-  # your answer here
+  friend[:favorite_food] = "sushi"
+  return friend
 end
