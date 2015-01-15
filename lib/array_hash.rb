a2d6f94d8bf79e959d12f810dd451446b8ab6290
Jeffrey Horn <jeffh@generalassemb.ly>
2015-01-14 23:54:41 -0500

    Improve question instructions
    See #28.

--- lib/array_hash.rb
+++ lib/array_hash.rb
@@ -1,35 +1,42 @@
 # Try to not use any reference for this one. If you need to use a reference,
 # please note such in a comment below your answer.
 # You'll still recieve credit, but it's useful for me to know.
+# For each question, your answer should work with any well-behaved input.
 
 # Question 1
-# On a new line, write code to insert the value `snake` at the end of the pets array.
+# On a new line, write code to insert the passed-in value at the end of the pets array.
 # Don't modify the original array creation line.
 def append_to_array(value)
   pets = ['cat', 'dog']
+  # your answer here
 end
 
 # Question 2
-# Write code to return the value of the key :age in the friend hash
+# Write code to return the value of the passed-in key from the friend hash.
 def get_key_value(key)
   friend = { name: "Chris", favorite_food: "Pizza", age: 27 }
+  # your answer here
 end
 
 # Question 3
-# Add a key of :hair_color with a value of "blue" to the friend hash
+# Add a key and value to the friend hash.
 def add_key_value_pair(key,value)
   friend = { name: "Chris", favorite_food: "Pizza", age: 27 }
+  # your answer here
 end
 
 # Question 4
-# Write code to retrieve the index of 'dog' in the pet array
+# Write code to retrieve the index of an element in the pet array.
 def find_index_of_array(value)
   pets = ['cat', 'dog']
+  # your answer here
 end
 
 
 # Question 5
-# Write code to change the value of :favorite_food in the friend hash to 'sushi'
+# Write code to change the value of a key in the friend hash to a different
+# value.
 def change_value_of_key(key,value)
   friend = { name: "Chris", favorite_food: "Pizza", age: 27 }
+  # your answer here
 end
